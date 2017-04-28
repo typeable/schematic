ff78b8efe7d42831ec65b702208869dceea26f16
Denis Redozubov <denis.redozubov@gmail.com>
2017-04-28 12:25:42 +0300

    TypeInType failg

--- src/Data/Schematic/Schema.hs
+++ src/Data/Schematic/Schema.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fprint-explicit-kinds #-}
 
 module Data.Schematic.Schema where
 
@@ -14,22 +15,22 @@ import Data.Vinyl.Functor
 import GHC.TypeLits (KnownNat, natVal, KnownSymbol, symbolVal, Symbol, Nat)
 
 singletons [d|
-    data Type
+    data Type els
       = Text
       | Number
       | Array
-      | Object
+      | Object els
       | Null
 
   |]
 
-type family CRepr (ty :: k) :: Type
+type family CRepr (ty :: k) :: Type els
 
 type family Repr (ty :: k1) :: k2 where
   Repr 'Text = String -- because of matchRegex
   Repr 'Number = Double
 
-class Constrained c ty | c -> ty where
+class Constrained c ty where
   predicate :: Repr ty -> Bool
 
 data LengthEqNat nat
@@ -40,47 +41,44 @@ instance KnownNat nat => Constrained (LengthEqNat nat) Text where
 type instance CRepr (LengthEqNat nat) = Number
 
 type family Verifiable (els :: k) (t :: z) :: Constraint where
-  Verifiable '[]        ty = ()
+  Verifiable '[]       ty = ()
   Verifiable (e : els) ty = (Constrained e (CRepr e), Verifiable els ty)
 
-data Exists f where
-  Exists :: f a -> Exists f
-
-data C els =
-  forall els ty. Verifiable els ty => C (Rec Proxy els)
-
-data Schema where
-  SchemaText :: Schema
-  SchemaNumber :: Schema
-  SchemaObjCons :: KnownSymbol label
+data Schema (ty :: Type k) where
+  SchemaText :: Verifiable cs Text => Rec Sing cs -> Schema Text
+  SchemaNumber :: Verifiable cs Number => Rec Sing cs -> Schema Number
+  SchemaObjCons :: (KnownSymbol label, Verifiable cs t)
     => Proxy label
-    -> Schema -- new elem
-    -> Schema -- tail
-    -> Schema
-  SchemaObjNil :: Schema
+    -> Rec Sing cs
+    -> Schema t -- new elem
+    -> Schema (Object tys) -- tail
+    -> Schema (Object (t ': tys))
+  SchemaObjNil :: Schema (Object '[])
 
 class Schematic ty where
-  rep :: Proxy ty -> Schema
-
--- | Signals it's appropriate as a top-level json type
--- class Schematic ty => TopLevel ty
+  type SchemaOf ty :: Type t
+  rep :: Proxy ty -> Schema (SchemaOf ty)
 
 data c :% s
 
 instance Schematic s => Schematic (c :% s) where
+  type SchemaOf (c :% s) = SchemaOf s
   rep _ = rep $ Proxy @s
 
 data NilObj
 
 instance Schematic NilObj where
+  type SchemaOf NilObj = Object '[]
   rep _ = SchemaObjNil
 
-data (:**) (label :: Symbol) e (s :: Type)
+data (:**) (label :: Symbol) e (s :: k)
 
 instance (KnownSymbol label, Schematic e, Schematic s)
   => Schematic (label :** e s) where
+  type SchemaOf (label :** e s) = SchemaOf s
   rep _ = SchemaObjCons
     (Proxy @label)
+    RNil
     (rep $ Proxy @e)
     (rep $ Proxy @s)
 
@@ -89,6 +87,9 @@ data JText
 instance Schematic JText where
   rep _ = SchemaText
 
--- type TestStruct
---   = "carrier" (JString :% LengthEqNat 2 :% Regexp "[a-zA-Z]+")
---   :& "booking_classes" (JArray :% (LengthArrLe 5) (JString :% Regexp "[a-zA-Z]{2}"))
+-- | Signals it's appropriate as a top-level json type
+-- class Schematic ty => TopLevel ty
+
+-- -- type TestStruct
+-- --   = "carrier" (JString :% LengthEqNat 2 :% Regexp "[a-zA-Z]+")
+-- --   :& "booking_classes" (JArray :% (LengthArrLe 5) (JString :% Regexp "[a-zA-Z]{2}"))
