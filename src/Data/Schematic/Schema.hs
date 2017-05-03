1836bc7c4cebfd1be3371fe51658e5cdf68bb6db
Denis Redozubov <denis.redozubov@gmail.com>
2017-05-03 16:37:49 +0300

    type-level objects to schema

--- src/Data/Schematic/Schema.hs
+++ src/Data/Schematic/Schema.hs
@@ -1,95 +1,138 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fprint-explicit-kinds #-}
 
 module Data.Schematic.Schema where
 
 import Data.Functor.Foldable
-import Data.Singletons.TH
 import Data.Kind hiding (Type)
 import Data.Proxy
+import Data.Singletons.TH
+import Data.Text (Text)
 import Data.Type.Equality
 import Data.Vinyl
 import Data.Vinyl.Functor
+import GHC.Exts
 import GHC.TypeLits (KnownNat, natVal, KnownSymbol, symbolVal, Symbol, Nat)
 
-singletons [d|
-    data Type els
-      = Text
-      | Number
-      | Array
-      | Object els
-      | Null
 
-  |]
+-- | Type Tags
+data JType
+  = JText
+  | JNumber
+  | JArray
+  | JObject
+  | JNull
 
-type family CRepr (ty :: k) :: Type els
+type family Repr (ty :: JType) :: * where
+  Repr 'JText = String -- because of matchRegex
+  Repr 'JNumber = Double
 
-type family Repr (ty :: k1) :: k2 where
-  Repr 'Text = String -- because of matchRegex
-  Repr 'Number = Double
-
-class Constrained c ty where
-  predicate :: Repr ty -> Bool
+class Constrained (ty :: JType) c where
+  cRep :: Proxy ty -> Proxy c -> CRepr ty
 
 data LengthEqNat nat
 
-instance KnownNat nat => Constrained (LengthEqNat nat) Text where
-  predicate n = natVal (Proxy @nat) == fromIntegral (length n)
+instance KnownNat nat => Constrained 'JText (LengthEqNat nat) where
+  cRep _ _ = LengthEq $ natVal (Proxy @nat)
 
-type instance CRepr (LengthEqNat nat) = Number
+data Field field jty schema
 
-type family Verifiable (els :: k) (t :: z) :: Constraint where
-  Verifiable '[]       ty = ()
-  Verifiable (e : els) ty = (Constrained e (CRepr e), Verifiable els ty)
+instance (Schematic jty, Verifiable (JT jty) cs, KnownSymbol field)
+  => Constrained JObject (Field field jty cs) where
+  cRep _ _ =
+    (symbolVal (Proxy @field), constructor (Proxy @jty) $ schema (Proxy @(JT jty)) (Proxy @cs))
 
-data Schema (ty :: Type k) where
-  SchemaText :: Verifiable cs Text => Rec Sing cs -> Schema Text
-  SchemaNumber :: Verifiable cs Number => Rec Sing cs -> Schema Number
-  SchemaObjCons :: (KnownSymbol label, Verifiable cs t)
-    => Proxy label
-    -> Rec Sing cs
-    -> Schema t -- new elem
-    -> Schema (Object tys) -- tail
-    -> Schema (Object (t ': tys))
-  SchemaObjNil :: Schema (Object '[])
+class Verifiable ty cs where
+  schema :: Proxy ty -> Proxy cs -> [CRepr ty]
+
+instance Verifiable JText '[] where
+  schema _ _ = []
+
+instance
+  (Constrained JText c, Verifiable JText cs) => Verifiable JText (c ': cs) where
+  schema _ _ = schema (Proxy @JText) (Proxy @cs) ++ schema (Proxy @JText) (Proxy @cs)
+
+instance Verifiable JNumber '[] where
+  schema _ _ = []
+
+instance
+  (Constrained JNumber c, Verifiable JNumber cs) => Verifiable JNumber (c ': cs) where
+  schema _ _ = schema (Proxy @JNumber) (Proxy @cs) ++ schema (Proxy @JNumber) (Proxy @cs)
+
+instance Verifiable JObject '[] where
+  schema _ _ = []
+
+instance
+  (Constrained JObject c, Verifiable JObject cs, KnownSymbol field)
+  => Verifiable JObject ((Field field jty c) ': cs) where
+  schema _ _ = cRep (Proxy @JObject) (Proxy @c) : schema (Proxy @JObject) (Proxy @cs)
+
+type family VerifiableFields (ty :: JType) (els :: [k]) :: Constraint where
+  VerifiableFields ty '[] = ()
+  VerifiableFields ty ((Field f jty s) ': elss)
+    = (Constrained jty s, VerifiableFields ty elss)
+
+type family CRepr (jty :: JType) :: * where
+  CRepr JText  = TextConstraint
+  CRepr JNumber = NumberConstraint
+  CRepr JObject = (String, Schema)
+
+data TextConstraint
+  = LengthEq Integer
+  | LengthLe Integer
+  | LengthGt Integer
+
+data NumberConstraint
+  = Le Integer
+  | Gt Integer
+  | Eq Integer
+
+data Schema
+  = SchemaText [TextConstraint]
+  | SchemaNumber [NumberConstraint]
+  | SchemaObject [(String, Schema)]
+  | SchemaArray Schema
+
+-- | Apply constraint for each element of list
+type family AllSatisfy (c :: k -> Constraint) (s :: [k]) :: Constraint where
+  AllSatisfy c '[]       = ()
+  AllSatisfy c (a ': as) = (c a, AllSatisfy c as)
 
 class Schematic ty where
-  type SchemaOf ty :: Type t
-  rep :: Proxy ty -> Schema (SchemaOf ty)
+  type JT ty :: JType
+  constructor :: Proxy ty -> [CRepr (JT ty)] -> Schema
 
-data c :% s
+data SText cs
 
-instance Schematic s => Schematic (c :% s) where
-  type SchemaOf (c :% s) = SchemaOf s
-  rep _ = rep $ Proxy @s
+instance (Verifiable JText cs) => Schematic (SText cs) where
+  type JT (SText cs) = JText
+  constructor _ = SchemaText
 
-data NilObj
+-- example
+type TextSchema = SText '[LengthEqNat 3]
 
-instance Schematic NilObj where
-  type SchemaOf NilObj = Object '[]
-  rep _ = SchemaObjNil
+data SNumber cs
 
-data (:**) (label :: Symbol) e (s :: k)
+instance (Verifiable JNumber cs) => Schematic (SNumber cs) where
+  type JT (SNumber cs) = JNumber
+  constructor _ = SchemaNumber
 
-instance (KnownSymbol label, Schematic e, Schematic s)
-  => Schematic (label :** e s) where
-  type SchemaOf (label :** e s) = SchemaOf s
-  rep _ = SchemaObjCons
-    (Proxy @label)
-    RNil
-    (rep $ Proxy @e)
-    (rep $ Proxy @s)
+data SObject els
 
-data JText
+instance (els ~ '[k], AllSatisfy Schematic els) => Schematic (SObject els) where
+  type JT (SObject els) = JObject
+  constructor _ = SchemaObject
 
-instance Schematic JText where
-  rep _ = SchemaText
+-- | Shows it's top-level definition according to the json-schema
+class TopLevel spec
 
--- | Signals it's appropriate as a top-level json type
--- class Schematic ty => TopLevel ty
+instance Schematic (SObject spec) => TopLevel (SObject els)
 
--- -- type TestStruct
--- --   = "carrier" (JString :% LengthEqNat 2 :% Regexp "[a-zA-Z]+")
--- --   :& "booking_classes" (JArray :% (LengthArrLe 5) (JString :% Regexp "[a-zA-Z]{2}"))
+-- toSchema :: forall spec cs. (Schematic spec, VerifiableFields (JT spec), TopLevel spec) => Proxy spec -> Schema
+-- toSchema pspec = constructor pspec $ schema (Proxy @(JT spec)) pspec
+
+type TestStruct = SObject '[ '("carrier", [SText '[LengthEqNat 3]]) ]
