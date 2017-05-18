760387be7a7d8012363539af16fad99eb37e81d3
Denis Redozubov <denis.redozubov@gmail.com>
2017-05-18 23:13:45 +0300

    singletons rewrite WIPw

--- src/Data/Aeson/Validation.hs
+++ src/Data/Aeson/Validation.hs
@@ -9,7 +9,6 @@ import Data.Functor.Foldable
 import Data.HashMap.Strict as H
 import Data.Maybe
 import Data.Proxy
-import Data.Schematic.Schema (TopLevel, toSchema, JT, Schematic)
 import Data.Schematic.Validation
 import Data.Singletons
 import Data.Singletons.Decide
@@ -22,64 +21,64 @@ import Data.Traversable
 
 infixl 3 <&&>
 
-validateConstraint
-  :: SDecide JType
-  => Sing jtype
-  -> CRepr jtype
-  -> Value
-  -> Maybe Bool
-validateConstraint s c = \case
-  J.String t -> case s %~ SJText of
-    Proved Refl -> pure $ case c of
-      LengthEq n -> (== n) . fromIntegral . T.length $ t
-      LengthLe n -> (<= n) . fromIntegral . T.length $ t
-      LengthGt n -> (> n) . fromIntegral . T.length $ t
-    Disproved _ -> mzero
-  J.Number i -> case s %~ SJNumber of
-    Proved Refl -> pure $ case c of
-      Eq n -> i == fromIntegral n
-      Le n -> i <= fromIntegral n
-      Gt n -> i > fromIntegral n
-    Disproved _ -> mzero
-  J.Array v -> case s %~ SJArray of
-    Proved Refl -> case c of
-      LengthArrEq n -> pure . (== fromIntegral n) . F.length $ v
-    Disproved _ -> Nothing
-  J.Null    -> Just True
-  _         -> Just False
+-- validateConstraint
+--   :: TopLevel spec
+--   => Sing spec
+--   -> CRepr spec
+--   -> Value
+--   -> Maybe Bool
+-- validateConstraint s c = \case
+--   J.String t -> case s %~ SJText of
+--     Proved Refl -> pure $ case fromSing c of
+--       TEq n -> (== n) . fromIntegral . T.length $ t
+--       TLe n -> (<= n) . fromIntegral . T.length $ t
+--       TGt n -> (> n) . fromIntegral . T.length $ t
+--     Disproved _ -> mzero
+--   J.Number i -> case s %~ SJNumber of
+--     Proved Refl -> pure $ case fromSing c of
+--       NEq n -> i == fromIntegral n
+--       NLe n -> i <= fromIntegral n
+--       NGt n -> i > fromIntegral n
+--     Disproved _ -> mzero
+--   J.Array v -> case s %~ SJArray of
+--     Proved Refl -> case fromSing c of
+--       AEq n -> pure . (== fromIntegral n) . F.length $ v
+--     Disproved _ -> Nothing
+--   J.Null    -> Just True
+--   _         -> Just False
 
-validateConstraints
-  :: SDecide JType
-  => Sing jtype
-  -> [CRepr jtype]
-  -> Value
-  -> Maybe Bool
-validateConstraints s cs v = and <$> traverse alg cs
-  where
-    alg c = validateConstraint s c v
+-- validateConstraints
+--   :: SDecide JType
+--   => Sing jtype
+--   -> [CRepr jtype]
+--   -> Value
+--   -> Maybe Bool
+-- validateConstraints s cs v = and <$> traverse alg cs
+--   where
+--     alg c = validateConstraint s c v
 
-validateJson
-  :: (SDecide JType, TopLevel spec)
-  => Proxy spec
-  -> Value
-  -> Maybe Bool
-validateJson ps v = let s = toSchema ps in validateBySchema s v
+-- validateJson
+--   :: (SDecide JType, TopLevel spec)
+--   => Sing spec
+--   -> Value
+--   -> Maybe Bool
+-- validateJson = validateBySchema . fromSing
 
-validateBySchema
-  :: SDecide JType
-  => Schema
-  -> Value
-  -> Maybe Bool
-validateBySchema s v = case (unfix s, v) of
-  (SchemaArray cs ss, jv@(J.Array v)) -> do
-    itself <- validateConstraints SJArray cs jv
-    content <- traverse (validateBySchema ss) v
-    pure $ itself && and content
-  (SchemaObject ocs, J.Object o)   -> do
-    fmap and $ for ocs $ \(fieldName, s) -> do
-      val <- H.lookup (T.pack fieldName) o
-      validateBySchema s val
-  (SchemaText cs, js@(J.String _)) -> validateConstraints SJText cs js
-  (SchemaNumber cs, jn@(J.Number _)) -> validateConstraints SJNumber cs jn
-  (SchemaNull, J.Null)             -> Just True
-  _                                  -> Nothing
+-- validateBySchema
+--   :: SDecide JType
+--   => Schema
+--   -> Value
+--   -> Maybe Bool
+-- validateBySchema s v = case (unfix s, v) of
+--   (SchemaArray cs ss, jv@(J.Array v)) -> do
+--     itself <- validateConstraints SJArray cs jv
+--     content <- traverse (validateBySchema ss) v
+--     pure $ itself && and content
+--   (SchemaObject ocs, J.Object o)   -> do
+--     fmap and $ for ocs $ \(fieldName, s) -> do
+--       val <- H.lookup (T.pack fieldName) o
+--       validateBySchema s val
+--   (SchemaText cs, js@(J.String _)) -> validateConstraints SJText cs js
+--   (SchemaNumber cs, jn@(J.Number _)) -> validateConstraints SJNumber cs jn
+--   (SchemaNull, J.Null)             -> Just True
+--   _                                  -> Nothing
