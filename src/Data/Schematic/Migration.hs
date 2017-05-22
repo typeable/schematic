bf3070ba731d71f8f3e20cf8df0aa1adb81ef565
Denis Redozubov <denis.redozubov@gmail.com>
2017-05-22 14:17:41 +0300

    WIP migrations

---
+++ src/Data/Schematic/Migration.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Data.Schematic.Migration where
+
+import Data.Kind
+import Data.Schematic.Path
+import Data.Schematic.Schema
+import Data.Schematic.Utils
+import Data.Singletons.Prelude
+import Data.Singletons.TypeLits
+
+
+data Action = AddKey Symbol Schema | Update Schema | DeleteKey
+
+data instance Sing (a :: Action) where
+  SAddKey
+    :: (Known (Sing n), Known (Sing s))
+    => Sing n
+    -> Sing s
+    -> Sing ('AddKey n s)
+  SUpdate :: Known (Sing s) => Sing s -> Sing ('Update s)
+  SDelete :: Sing 'DeleteKey
+
+instance (Known (Sing n), Known (Sing s)) => Known (Sing ('AddKey n s)) where
+  known = SAddKey known known
+instance (Known (Sing s)) => Known (Sing ('Update s)) where known = SUpdate known
+instance Known (Sing 'DeleteKey) where known = SDelete
+
+data Diff = DiffSchema [PathSegment] Action
+
+data instance Sing (diff :: Diff) where
+  SDiff
+    :: (Known (Sing jp), Known (Sing a))
+    => Sing (jp :: [PathSegment])
+    -> Sing (a :: Action)
+    -> Sing ('DiffSchema jp a)
+
+type Revision = Symbol
+
+data Migration = Migration Revision [Diff]
+
+data instance Sing (m :: Migration) where
+  SMigration
+    :: (KnownSymbol r, Known (Sing ds))
+    => Sing r
+    -> Sing ds
+    -> Sing ('Migration r ds)
+
+data Versioned = Versioned
+  Schema      -- base version
+  [Migration] -- a bunch of migrations
+
+data instance (Sing (v :: Versioned)) where
+  SVersioned
+    :: (Known (Sing s), Known (Sing ms))
+    => Sing s
+    -> Sing ms
+    -> Sing ('Versioned s ms)
+
+type family Typed (s :: Schema) (ps :: PathSegment) :: Constraint where
+  Typed ('SchemaObject '[]) ('Key fn)               = FalseConstraint fn
+  Typed ('SchemaObject ( '(fn, s) ': tl)) ('Key fn) = ()
