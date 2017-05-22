cc8a8c95f4291fa65b8564ef65ab6e2ee8454276
Denis Redozubov <denis.redozubov@gmail.com>
2017-05-22 18:25:51 +0300

    Add orphan instances module

---
+++ src/Data/Schematic/Instances.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
+module Data.Schematic.Instances where
+
+import Data.Scientific
+import Data.Vector as V
+import Data.Vinyl
+import Test.SmallCheck.Series
+
+
+instance Monad m => Serial m (Rec f '[]) where
+  series = cons0 RNil
+
+instance (Serial m (f a), Serial m (Rec f as), Monad m)
+  => Serial m (Rec f (a ': as)) where
+  series = cons2 (:&)
+
+instance Serial m a => Serial m (V.Vector a) where
+  series = V.fromList <$> series
+
+instance Monad m => Serial m Scientific where
+  series = scientific <$> series <*> series
