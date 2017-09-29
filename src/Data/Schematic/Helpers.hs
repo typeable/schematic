fced3b905eddae2514318a77105640fadb79093d
Denis Redozubov <denis.redozubov@gmail.com>
2017-09-29 17:06:26 +0300

    attempt to rebuild iso8601 validation

--- src/Data/Schematic/Helpers.hs
+++ src/Data/Schematic/Helpers.hs
@@ -1,13 +1,56 @@
 module Data.Schematic.Helpers where
 
 import Data.Schematic.Schema
+import GHC.TypeLits
+
 
 type UUIDRegex =
   'TRegex "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
 
 type IsUUID = '[UUIDRegex]
 
-type ISO8601Regex =
-  'TRegex "(\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d+([+-][0-2]\\d:[0-5]\\d|Z))|(\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d([+-][0-2]\\d:[0-5]\\d|Z))|(\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d([+-][0-2]\\d:[0-5]\\d|Z))"
+-- [ "1985-04-12T23:20:50.52Z"
+-- , "1996-12-19T16:39:57-08:00"
+-- , "1990-12-31T23:59:60Z"
+-- , "1990-12-31T15:59:60-08:00"
+-- , "1937-01-01T12:00:27.87+00:20"
+-- ]
 
-type IsDateTime = '[ISO8601Regex]
+-- components
+type ISO8601Date = "[1-9][0-9]{3}-(0[1-9]|1[0-2])-[0-3][0-9]"
+
+type ISO8601Time = "(?:[0-2][0-9]:[0-2][0-9]:[0-6][0-9])?"
+
+type ISO8601DateTime = AppendSymbol ISO8601Date (AppendSymbol "T" ISO8601Time)
+
+type ISO8601UTC = "(Z|\\+00:00)"
+
+type ISO8601DateTimeUTC =
+  AppendSymbol ISO8601Date (AppendSymbol "T" (AppendSymbol ISO8601Time ISO8601UTC))
+
+type ISO8601TZ = "\\+[0-1][0-9]:[0-6][0-9]"
+
+type ISO8601DateTimeZoned =
+  AppendSymbol ISO8601Date (AppendSymbol "T" (AppendSymbol ISO8601Time ISO8601TZ))
+
+-- regexes
+type ISO8601DateRegex = 'TRegex ISO8601Date
+
+type ISO8601TimeRegex = 'TRegex ISO8601Time
+
+type ISO8601DateTimeRegex = 'TRegex ISO8601DateTime
+
+type ISO8601DateTimeRegexUTC = 'TRegex ISO8601DateTimeUTC
+
+type ISO8601DateTimeRegexZoned = 'TRegex ISO8601DateTimeZoned
+
+-- constraints
+type IsDate = '[ISO8601DateRegex]
+
+type IsTime = '[ISO8601TimeRegex]
+
+type IsDateTime = '[ISO8601DateTimeRegex]
+
+type IsZonedDateTime = '[ISO8601DateTimeRegexZoned]
+
+type IsUTCDateTime = '[ISO8601DateTimeRegexUTC]
--- test/HelpersSpec.hs
+++ test/HelpersSpec.hs
@@ -24,16 +24,34 @@ type UUIDSchema = 'SchemaObject '[ '("uuid", 'SchemaText IsUUID) ]
 uuid :: Text
 uuid = "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"
 
-type ISO8601Schema = 'SchemaObject '[ '("datetime", 'SchemaText IsDateTime) ]
+type ISO8601DateSchema = 'SchemaObject '[ '("date", 'SchemaText IsDate) ]
+
+iso8601dates :: [Text]
+iso8601dates = [ "1985-04-12" ]
+
+invalidIso8601dates :: [Text]
+invalidIso8601dates = [ "1999-13-12" ]
+
+type ISO8601DateTimeSchema = 'SchemaObject '[ '("datetime", 'SchemaText IsDateTime) ]
 
 -- | Taken from ISO8601 RFC 3339
 iso8601datetimes :: [Text]
 iso8601datetimes =
+  iso8601dates <>
   [ "1985-04-12T23:20:50.52Z"
   , "1996-12-19T16:39:57-08:00"
   , "1990-12-31T23:59:60Z"
   , "1990-12-31T15:59:60-08:00"
   , "1937-01-01T12:00:27.87+00:20"
+  ]
+
+invalidIso8601datetimes :: [Text]
+invalidIso8601datetimes =
+  [ "1985-04-29T23:20:50.52Z"
+  , "1996-12-129T16:39:57-08:00"
+  , "1990-2-31T23:59:60Z"
+  , "1990-12-31T15:59:70"
+  , "1937-01-01T12:00:27.87+24:20"
   ]
 
 spec :: Spec
@@ -51,11 +69,30 @@ spec = do
       (decodeAndValidateJson incorrectJson :: ParseResult (JsonRepr UUIDSchema))
         `shouldSatisfy` isValidationError
 
+  describe "ISO8601 date: " $ do
+    for_ iso8601dates $ \dt -> do
+      it ("validates correct date - " <> dt ^. unpacked) $ do
+        let json = "{\"date\": \"" <> (dt ^. unpacked . packedChars) <> "\" }"
+        (decodeAndValidateJson json :: ParseResult (JsonRepr ISO8601DateSchema))
+          `shouldSatisfy` isValid
+
+    for_ invalidIso8601dates $ \dt -> do
+      it ("fails to validate incorrect date - " <> dt ^. unpacked) $ do
+        let json = "{\"date\": \"" <> (dt ^. unpacked . packedChars) <> "\" }"
+        (decodeAndValidateJson json :: ParseResult (JsonRepr ISO8601DateSchema))
+          `shouldSatisfy` isValidationError
+
   describe "ISO8601 datetime: " $ do
     for_ iso8601datetimes $ \dt -> do
       it ("validates correct datetime - " <> dt ^. unpacked) $ do
         let json = "{\"datetime\": \"" <> (dt ^. unpacked . packedChars) <> "\" }"
-        (decodeAndValidateJson json :: ParseResult (JsonRepr ISO8601Schema))
+        (decodeAndValidateJson json :: ParseResult (JsonRepr ISO8601DateTimeSchema))
+          `shouldSatisfy` isValid
+
+    for_ invalidIso8601datetimes $ \dt -> do
+      it ("fails to validate incorrect datetime - " <> dt ^. unpacked) $ do
+        let json = "{\"datetime\": \"" <> (dt ^. unpacked . packedChars) <> "\" }"
+        (decodeAndValidateJson json :: ParseResult (JsonRepr ISO8601DateTimeSchema))
           `shouldSatisfy` isValidationError
 
 main :: IO ()
