f648e38e6653aea55b4de24778368c0ad468b5b4
Denis Redozubov <denis.redozubov@gmail.com>
2017-10-02 16:01:00 +0300

    Add constraints

--- src/Data/Schematic/JsonSchema.hs
+++ src/Data/Schematic/JsonSchema.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Data.Schematic.JsonSchema
   ( toJsonSchema
@@ -8,9 +9,10 @@ module Data.Schematic.JsonSchema
   ) where
 
 import Control.Monad.State.Strict
-import Data.Foldable
+import Data.Aeson as J
+import Data.Foldable as F
 import Data.HashMap.Strict as H
-import Data.List.NonEmpty
+import Data.List.NonEmpty as NE
 import Data.Schematic.Schema as S
 import Data.Singletons
 import Data.Text
@@ -19,24 +21,39 @@ import JSONSchema.Validator.Draft4 as D4
 
 
 draft4 :: Text
-draft4 = "http://Jason-schema.org/draft-04/schema#"
+draft4 = "http://json-schema.org/draft-04/schema#"
 
--- FIXME: implement all this later
 textConstraint :: DemotedTextConstraint -> State D4.Schema ()
-textConstraint (DTEq _) = pure ()
-textConstraint (DTLt _) = pure ()
-textConstraint (DTLe _) = pure ()
-textConstraint (DTGt _) = pure ()
-textConstraint (DTGe _) = pure ()
-textConstraint (DTRegex _) = pure ()
-textConstraint (DTEnum _) = pure ()
+textConstraint (DTEq n) = modify $ \s -> s
+  { _schemaMinLength = pure $ fromIntegral n
+  , _schemaMaxLength = pure $ fromIntegral n }
+textConstraint (DTLt n) = modify $ \s -> s
+  { _schemaMaxLength = pure . fromIntegral $ n + 1 }
+textConstraint (DTLe n) = modify $ \s -> s
+  { _schemaMaxLength = pure . fromIntegral $ n }
+textConstraint (DTGt n) =
+  let n' = if n == 0 then 0 else n - 1
+  in modify $ \s -> s { _schemaMinLength = pure . fromIntegral $ n' }
+textConstraint (DTGe n) = modify $ \s -> s
+  { _schemaMinLength = pure . fromIntegral $ n }
+textConstraint (DTRegex r) = modify $ \s -> s { _schemaPattern = pure r }
+textConstraint (DTEnum ss) =
+  let ss' = if F.length ss == 0 then [] else NE.fromList $ J.String <$> ss
+  in modify $ \s -> s { _schemaEnum = pure ss' }
 
 numberConstraint :: DemotedNumberConstraint -> State D4.Schema ()
-numberConstraint (DNLe _) = pure ()
-numberConstraint (DNLt _) = pure ()
-numberConstraint (DNGt _) = pure ()
-numberConstraint (DNGe _) = pure ()
-numberConstraint (DNEq _) = pure ()
+numberConstraint (DNLe n) = modify $ \s -> s
+  { _schemaMaximum = pure . fromIntegral $ n }
+numberConstraint (DNLt n) = modify $ \s -> s
+  { _schemaMaximum = pure . fromIntegral $ n + 1 }
+numberConstraint (DNGt n) = modify $ \s -> s
+  { _schemaMinimum = pure . fromIntegral $ n }
+numberConstraint (DNGe n) =
+  let n' = if n == 0 then 0 else n - 1
+  in modify $ \s -> s { _schemaMinimum = pure . fromIntegral $ n' }
+numberConstraint (DNEq n) = modify $ \s -> s
+  { _schemaMinimum = pure $ fromIntegral n
+  , _schemaMaximum = pure $ fromIntegral n }
 
 arrayConstraint :: DemotedArrayConstraint -> State D4.Schema ()
 arrayConstraint (DAEq _) = pure ()
