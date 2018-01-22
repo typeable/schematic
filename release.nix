060ed7cc3082064b85f387fe682547092eb4335f
Dmitry Bushev <bushevdv@gmail.com>
2018-01-22 10:57:54 +0300

    Upd release.nix

--- release.nix
+++ release.nix
@@ -3,26 +3,30 @@
 }:
 
 let
-  nixpkgs = import <nixpkgs> {};
-  inherit (nixpkgs) pkgs lib;
+  _nixpkgs = import <nixpkgs> {};
+  inherit (_nixpkgs) pkgs lib;
 
-  stackageOverlayPath = pkgs.fetchFromGitHub (lib.importJSON ./stackage-overlay.json);
+  stackageOverlayPath = pkgs.fetchFromGitHub {
+    owner = "typeable";
+    repo = "nixpkgs-stackage";
+    rev = "abe0a03ba2bde609b0707ce45301ec836cb3ed29";
+    sha256 = "0ksdk83zbsgvs30izwsfyjh4xxwz8dsn9a5igrs3pjap4whpzqfv";
+  };
 
   nixpkgsArgs = {
     overlays = [ (import stackageOverlayPath) ];
-    #config = { allowUnfree = false; inHydra = true; };
   };
 
   release = import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit nixpkgsArgs supportedSystems; };
 
-  mkSchematic = system:
+  mkStackage = system:
     let
       nixpkgs = release.pkgsFor system;
       stackage = nixpkgs.haskell.packages.stackage.lib.callStackage2nix "schematic" schematicSrc { inherit nixpkgs; };
     in
-      stackage.schematic;
+      stackage;
 
 in
   lib.genAttrs supportedSystems (system: {
-    schematic = pkgs.haskell.lib.triggerRebuild (mkSchematic system) 0;
+    inherit (mkStackage system) schematic;
   })
